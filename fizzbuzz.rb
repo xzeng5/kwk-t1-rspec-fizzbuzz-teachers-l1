9f4ad238aac6700f3c06bb3c868278d92daf0212
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-01 03:28:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,4 @@
 def fizzbuzz(int)
   if int % 3 == 0
     puts "Fizz"
-  elseif int % 5 == 0
-    puts "Buzz"
-  elseif int % 3 == 0 && int % 5 == 0
-    puts "FizzBuzz"
-  else
-    puts "nil"
 end
